ed1aa5de9e79893a58c20e04511d3fb35e7c1c53
Saskia van Barthold <saskia.vanb@gmail.com>
2020-05-24 14:55:43 +0200

    additional exercises

---
+++ Python/exercises/108_bizzuuu_exercise.py
@@ -0,0 +1,30 @@
+# Write a bizz and zzuu game ##project
+
+# as a user I should be able prompted for a number, as the program will print all the number up to and inclusing said number while following the constraints / specs. (bizz and buzz for multiples or 3 and 5)
+# number= str(input("Choose a number:"))
+# print[O,number]
+# for x in number:
+#     if (number % 5) == "0":
+#         print("buzz")
+#     elif (number % 3)== "0":
+#         print("bizz")
+#     else:
+#         print("You lost")
+
+# As a user I should be able to keep giving the program different numbers and it will calculate appropriately until you use the key word: 'penpinapplespen'
+
+
+# write a program that take a number
+# prints back each individual number, but
+    # if it is a multiple of 3 it returns bizz
+    # if a multiple of 5 it return zzuu
+    # if a multiple of 3 and 5 it return bizzzzuu
+
+
+#  EXTRA:
+#  Make it functional
+#  make it so I can it so it can work with 4 and 9 insted of 3 and 5.
+    #      make it so it can work with any number!
+
+first_name="Ada"
+print(first_name, "is learning Python")
